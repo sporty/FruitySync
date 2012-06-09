0822691c2b2044933a93b84dc7cccc3a8857385a
ryo takahashi <rt.sporty@gmail.com>
2012-06-09 16:30:49 +0900

    twitterへのリンクを追加。

--- djproject/djproject/core/fb.py
+++ djproject/djproject/core/fb.py
@@ -11,6 +11,7 @@ import urllib2
 import json
 import random
 import re
+import random
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -35,6 +36,87 @@ class Wall(object):
     """
     facebookウォールに他のSNSからの書き込みを同期する
     """
+
+    def get_profile(self):
+        """
+        """
+        graph = facebook.GraphAPI(self.facebook_access_key)
+
+        profile = graph.get_object("me")
+        return profile
+        print "profile "+"-"*50
+        pp.pprint(profile)
+
+    def get_friends(self):
+        """
+        """
+        graph = facebook.GraphAPI(self.facebook_access_key)
+
+        friends = graph.get_connections("me", "friends")
+        return friends
+        print "friends "+"-"*50
+        pp.pprint(friends)
+
+    def get_albums(self):
+        """
+        """
+        graph = facebook.GraphAPI(self.facebook_access_key)
+
+        albums = graph.get_object("me/albums")
+        return albums
+        print "albums "+"-"*50
+        pp.pprint(albums)
+
+    def get_feeds(self):
+        """
+        """
+        graph = facebook.GraphAPI(self.facebook_access_key)
+
+        feeds = graph.get_object("me/feed")
+        return feeds
+
+    def get_twitter_profile(self):
+        """
+        """
+        # twitter api
+        auth = tweepy.OAuthHandler(self.twitter_consumer_key, self.twitter_consumer_secret)
+        auth.set_access_token(self.twitter_access_key, self.twitter_access_secret)
+        api = tweepy.API(auth_handler=auth)
+
+        me = api.me() # 自身のUserクラス取得
+        return me
+
+        for key in dir(twitter_profile):
+            if key.startswith("__"):
+                continue
+
+            try:
+                method = getattr(twitter_profile, key)
+                print key+" : ",
+                pp.pprint(method())
+            except:
+                print key+" is uncallable.",
+
+        pp.pprint(twitter_profile.id)
+        pp.pprint(twitter_profile.id_str)
+        pp.pprint(twitter_profile.screen_name)
+        pp.pprint(twitter_profile.default_profile)
+
+    def _get_twitter_screen_name(self):
+        """
+        """
+        profile = self.get_twitter_profile()
+        return profile.screen_name
+
+    def _get_action_link(self):
+        """
+        """
+        # twitterアカウントへのリンク
+        screen_name = self._get_twitter_screen_name()
+        action_name = u"@%s on Twitter" % (screen_name, )
+        action_link = u"https://twitter.com/#!/%s" % (screen_name, )
+        action = json.dumps({u'name': action_name, u'link': action_link})
+        return action
 
     def _find_status(self, since_id, since_datetime, except_ids, except_clients=None, check_all_status=False):
         """
@@ -183,7 +265,25 @@ class Wall(object):
 
         return album_id
 
-    def _post_wall(self, posts, album_id):
+    def put_feed(self, message, icon=None):
+        """
+        """
+        graph = facebook.GraphAPI(self.facebook_access_key)
+
+        action = self._get_action_link()
+
+        graph.put_object("me", "feed", message=message.encode('utf-8'), actions=action, icon=icon)
+
+    def put_photo(self, message, photo_filename, album_id=None, icon=None):
+        """
+        """
+        graph = facebook.GraphAPI(self.facebook_access_key)
+
+        action = self._get_action_link()
+
+        graph.put_photo(file(str(photo_filename)), message=message.encode('utf-8'), album_id=album_id, actions=action, icon=icon)
+
+    def _post_wall(self, posts, album_id, icon):
         """
         ウォールに投稿する。
 
@@ -196,9 +296,6 @@ class Wall(object):
         if not self.facebook_access_key:
             raise eh.FacebookAuthError(u"facebookの認証を行ってください")
 
-        # facebook api
-        graph = facebook.GraphAPI(self.facebook_access_key)
-
         for post in posts:
             print post
             id = post['id']
@@ -208,12 +305,12 @@ class Wall(object):
 
             try:
                 if post_type == "wall":
-                    graph.put_object("me", "feed", message=message.encode('utf-8'))
+                    self.put_feed(message, icon)
                 elif post_type == "photo":
                     for photo in photos:
                         print photo
                         if os.path.isfile(photo):
-                            graph.put_photo(file(str(photo)), message=message.encode('utf-8'), album_id=album_id)
+                            self.put_photo(message, photo, album_id, icon)
                         else:
                             print u"写真が無いよ"
                 else:
@@ -233,7 +330,7 @@ class Wall(object):
 
         return result
 
-    def sync_twitter(self, since_id=None, since_datetime=None, except_ids=[], except_clients=[], album_name=None, check_all_status=False, dry=False):
+    def sync_twitter(self, since_id=None, since_datetime=None, except_ids=[], except_clients=[], album_name=None, check_all_status=False, icon=None, dry=False):
         """
         twitterからfacebookに同期
 
@@ -273,7 +370,7 @@ class Wall(object):
         #facebookに書き込み 
         if not dry:
             try:
-                result = self._post_wall(posts, album_id)
+                result = self._post_wall(posts, album_id, icon)
             except:
                 raise
 
@@ -313,7 +410,16 @@ if __name__ == "__main__":
             TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET,
             twitter_access_key, twitter_access_secret
             )
+
+    #fb_wall.put_feed(unicode(random.random())*10)
+
+    feeds = fb_wall.get_feeds()
+    print "feeds "+"-"*50
+    pp.pprint(feeds)
+
+    '''
     fb_wall.sync_twitter()
+    '''
 
 
 # EOF

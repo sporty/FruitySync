4280980c73b267e78cf3682d9ab45bddced6c535
ryo takahashi <rt.sporty@gmail.com>
2012-11-04 17:27:04 +0900

    サインアップフォーム関連を修正。
    サインアップフォームを修正。
    データバリデートを修正。

--- djproject/djproject/sync/forms.py
+++ djproject/djproject/sync/forms.py
@@ -2,6 +2,8 @@
 # coding=utf-8
 
 from django import forms
+from django.contrib.auth.models import User
+
 
 class SignupForm(forms.Form):
     """
@@ -38,5 +40,28 @@ class SignupForm(forms.Form):
                     widget=forms.PasswordInput,
                     label=u'パスワード再入力' 
                 )
-        
+
+    def clean(self):
+        """
+        is_validが呼ばれたときの処理
+        """
+
+        cleaned_data = super(SignupForm, self).clean()
+
+        email = cleaned_data.get("email")
+        try:
+            account = User.objects.get(username=email)
+            if account:
+                raise forms.ValidationError(u"指定されたメールアドレスは既に登録済みです")
+        except:
+            pass
+
+        password_1 = cleaned_data.get("password")
+        password_2 = cleaned_data.get("password_reply")
+        if not password_1 == password_2:
+            raise forms.ValidationError(u"パスワード入力に誤りがあります")
+
+        # Always return the full collection of cleaned data.
+        return cleaned_data       
+
 # EOF

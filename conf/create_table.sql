b87e28e831f6ffa28f6171a4fe3894af42453abe
ryo takahashi <rt.sporty@gmail.com>
2013-02-17 01:55:22 +0900

    デプロイを実装

---
+++ conf/create_table.sql
@@ -0,0 +1,3 @@
+create database db_fsync default character set utf8;
+grant all on db_fsync.* to fsync identified by 'fsync0623';
+flush privileges;
--- fabfile.py
+++ fabfile.py
@@ -2,13 +2,14 @@
 # coding=utf-8
 
 """
-サーバー管理用fabricスクリプト
+デプロイ用fabricスクリプト
 """
 
 import os
 import datetime
 import csv
 import re
+
 import fabric.api
 from fabric.api import lcd, local, get, cd, prefix, run, abort
 from fabric.contrib.console import confirm
@@ -16,29 +17,52 @@ from fabric.decorators import task
 from fabric.colors import green, red
 
 
-env.user = "fsync"
 '''
+env.user = "fsync"
 env.hosts = ['', ]
 env.password = "J"
 env.key_filename = [os.path.expanduser('~/.ssh/secret_key')]
 '''
 
-@task
-def host_name():
-    """
-    ホスト名を表示
-    """
-    local("uname -a")
-    run("uname -a")
+venv_basedir = "/var/gunicorn/venvs"
+app_basedir = "/var/gunicorn/apps"
 
 
 @task
-def setup(local_dir="backup"):
+def migration(app="fsync", djangoapps=["sync", ]):
     """
-    セットアップ
+    データベースのマイグレーション
     """
 
-    pass
+    # venvの切り替え
+    venv_dirname = os.path.join(venv_basedir, app)
+    # venv環境セットアップ
+    with prefix("source "+os.path.join(venv_dirname, "bin/activate")):
+        # アプリケーション更新
+        app_dirname = os.path.join(app_basedir, app)
+        with cd(app_dirname):
+            # マイグレーション
+            run("python djproject/manage.py syncdb")
+            for djangoapp in djangoapps:
+                run("python djproject/manage.py migrate %s" % (djangoapp, ))
+
+
+@task
+def create_table(app="fsync"):
+    """
+    データベース初期設定
+    """
+
+    # テーブル作成用sqlファイル
+    confs = [
+        "conf/create_table.sql",
+    ]
+
+    app_dirname = os.path.join(app_basedir, app)
+    with cd(app_dirname):
+        for conf in confs:
+            run("mysql -u root -p < %s" % (conf, ))
+
 
 @task
 def deploy(app="fsync", repo="https://github.com/sporty/fruity-sync.git"):
@@ -46,24 +70,23 @@ def deploy(app="fsync", repo="https://github.com/sporty/fruity-sync.git"):
     デプロイ
     """
 
-    print(green("deploy "+app))
-
-    venv_basedir = "/var/gunicorn/venvs"
-    app_basedir = "/var/gunicorn/apps"
+    #print(green("deploy "+app))
 
     # venvの切り替え
     venv_dirname = os.path.join(venv_basedir, app)
-    if run("test -d "+venv_dirname):
-        run("virtualenv --distribute "+venv_dirname)
+    with fabric.api.settings(warn_only=True):
+        if run("test -d "+venv_dirname).failed:
+            run("virtualenv --distribute "+venv_dirname)
 
     # venv環境セットアップ
     with prefix("source "+os.path.join(venv_dirname, "bin/activate")):
 
         # アプリケーション更新
         app_dirname = os.path.join(app_basedir, app)
-        if run("test -d "+app_dirname):
-            # 取得
-            run("git clone %s %s" % (repo, app_dirname))
+        with fabric.api.settings(warn_only=True):
+            if run("test -d %s" %(app_dirname, )).failed:
+                # 取得
+                run("git clone %s %s" % (repo, app_dirname))
 
         with cd(app_dirname):
             # 最新のファイルに更新
@@ -72,34 +95,23 @@ def deploy(app="fsync", repo="https://github.com/sporty/fruity-sync.git"):
             # モジュールの更新
             run("pip install -r requirements.txt")
 
-            # マイグレーション
-            run("python djproject/manage.py syncdb")
-            run("python djproject/manage.py migrate sync")
-
 
 @task
 def start(app="fsync"):
     """
     サーバー起動または再起動
     """
-    venv_basedir = "/var/gunicorn/venvs"
-    app_basedir = "/var/gunicorn/apps"
 
     # venvの切り替え
     venv_dirname = os.path.join(venv_basedir, app)
-    if run("test -d "+venv_dirname):
-        error()
 
     # venv環境セットアップ
     with prefix("source "+os.path.join(venv_dirname, "bin/activate")):
 
         app_dirname = os.path.join(app_basedir, app, "djproject")
-        if run("test -d "+app_dirname):
-            error()
-
         with cd(app_dirname):
             # サーバーの起動
-            run("gunicorn --conf ../gunicorn.conf.py djproject.wsgi:application")
+            run("gunicorn --conf ../conf/gunicorn.conf.py djproject.wsgi:application")
 
 @task
 def reload(app="fsync"):

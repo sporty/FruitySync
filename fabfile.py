6ae79529eb31f4caac364ca8bf887fd65ee5a2b1
ryo takahashi <rt.sporty@gmail.com>
2013-03-05 02:13:14 +0900

    整理。bootstrap作成

--- fabfile.py
+++ fabfile.py
@@ -24,41 +24,58 @@ app_basedir = "/var/gunicorn/apps"
 
 
 @task
-def migration(app="fsync", djangoapps=["sync", ]):
+def setup_logrotate(app="fsync"):
     """
-    データベースのマイグレーション
+    logrotateの設定
     """
 
-    # venvの切り替え
-    venv_dirname = os.path.join(venv_basedir, app)
-    # venv環境セットアップ
-    with prefix("source "+os.path.join(venv_dirname, "bin/activate")):
-        # アプリケーション更新
-        app_dirname = os.path.join(app_basedir, app)
-        with cd(app_dirname):
-            # マイグレーション
-            run("python djproject/manage.py syncdb")
-            for djangoapp in djangoapps:
-                run("python djproject/manage.py migrate %s" % (djangoapp, ))
+    # 設定ファイルのコピー
+    env_confs = {
+        "dev.smiletechnology.jp": [
+            ("conf/dev/logrotate/fsync",
+                "/etc/logrotate.d/fsync"),
+        ],
+        "smiletechnology.jp": [
+            ("conf/prod/logrotate/fsync",
+                "/etc/logrotate.d/fsync"),
+        ],
+    }
+
+    hostname = fabric.api.env.host
+    confs = env_confs[hostname]
+
+    app_dirname = os.path.join(app_basedir, app)
+    with cd(app_dirname):
+        for conf in confs:
+            run("cp %s %s" % (conf[0], conf[1]))
 
 
 @task
-def create_user(app="fsync"):
+def setup_cron(app="fsync", disable=False):
     """
-    実行ユーザー作成
+    crontabの設定
     """
-    username = app
-    groupname = app
 
-    with fabric.api.settings(warn_only=True):
-        # グループ作成
-        if run("groupadd %s" % (groupname, )).failed:
-            pass
+    env_confs = {
+        "dev.smiletechnology.jp": [
+            "conf/dev/cron.txt",
+        ],
+        "smiletechnology.jp": [
+            "conf/prod/cron.txt",
+        ],
+    }
 
-        # ユーザー作成
-        if run("id %s" % (username, )).failed:
-            run("adduser -g %s %s" % (groupname, username))
-            run("passwd %s" % (username, ))
+    hostname = fabric.api.env.host
+    confs = env_confs[hostname]
+
+    cronfile = confs[0]
+
+    app_dirname = os.path.join(app_basedir, app)
+    with cd(app_dirname):
+        if disable==True:
+            run("crontab -u fsync -r")
+        else:
+            run("crontab -u fsync %s" % (cronfile, ))
 
 
 @task
@@ -123,75 +140,86 @@ def setup_nginx(app="fsync"):
 
 
 @task
-def setup_logrotate(app="fsync", disable=False):
-    """
-    logrotateの設定
-    """
-
-    # 設定ファイルのコピー
-    env_confs = {
-        "dev.smiletechnology.jp": [
-            ("conf/dev/logrotate/fsync",
-                "/etc/logrotate.d/fsync"),
-        ],
-        "smiletechnology.jp": [
-            ("conf/prod/logrotate/fsync",
-                "/etc/logrotate.d/fsync"),
-        ],
-    }
-
-    hostname = fabric.api.env.host
-    confs = env_confs[hostname]
-
-    app_dirname = os.path.join(app_basedir, app)
-    with cd(app_dirname):
-        for conf in confs:
-            run("cp %s %s" % (conf[0], conf[1]))
-
-
-@task
-def setup_cron(app="fsync", disable=False):
-    """
-    crontabの設定
-    """
-
-    env_confs = {
-        "dev.smiletechnology.jp": [
-            "conf/dev/cron.txt",
-        ],
-        "smiletechnology.jp": [
-            "conf/prod/cron.txt",
-        ],
-    }
-
-    hostname = fabric.api.env.host
-    confs = env_confs[hostname]
-
-    cronfile = confs[0]
-
-    app_dirname = os.path.join(app_basedir, app)
-    with cd(app_dirname):
-        if disable==True:
-            run("crontab -u fsync -r")
-        else:
-            run("crontab -u fsync %s" % (cronfile, ))
-
-
-@task
-def create_table(app="fsync"):
+def setup_database(app="fsync"):
     """
     データベース初期設定
     """
 
     # テーブル作成用sqlファイル
     confs = [
-        "conf/create_table.sql",
+        "conf/common/create_table.sql",
     ]
 
     app_dirname = os.path.join(app_basedir, app)
-    with cd(app_dirname):
-        for conf in confs:
-            run("mysql -u root -p < %s" % (conf, ))
+    with fabric.api.settings(warn_only=True):
+        with cd(app_dirname):
+            for conf in confs:
+                run("mysql -u root -p < %s" % (conf, ))
+
+
+@task
+def setup_user(app="fsync"):
+    """
+    実行ユーザー作成
+    """
+    username = app
+    groupname = app
+
+    with fabric.api.settings(warn_only=True):
+        # グループ作成
+        if run("groupadd %s" % (groupname, )).failed:
+            pass
+
+        # ユーザー作成
+        if run("id %s" % (username, )).failed:
+            run("adduser -g %s %s" % (groupname, username))
+            run("passwd %s" % (username, ))
+
+
+@task
+def setup_all(app="fsync"):
+    """
+    全セットアップ
+    """
+
+    setup_user(app)
+    setup_database(app)
+    setup_nginx(app)
+    setup_supervisor(app)
+    setup_cron(app)
+    setup_logrotate(app)
+
+
+@task
+def migration(app="fsync", djangoapps=["sync", ]):
+    """
+    データベースのマイグレーション
+    """
+    env_confs = {
+        "dev.smiletechnology.jp": [
+            "djproject.settings_dev",
+        ],
+        "smiletechnology.jp": [
+            "djproject.settings_prod",
+        ],
+    }
+
+    hostname = fabric.api.env.host
+    confs = env_confs[hostname]
+
+    settings_file = confs[0]
+
+    # venvの切り替え
+    venv_dirname = os.path.join(venv_basedir, app)
+    # venv環境セットアップ
+    with prefix("source "+os.path.join(venv_dirname, "bin/activate")):
+        # アプリケーション更新
+        app_dirname = os.path.join(app_basedir, app)
+        with cd(app_dirname):
+            # マイグレーション
+            run("python djproject/manage.py syncdb --settings=%s" % (settings_file, ))
+            for djangoapp in djangoapps:
+                run("python djproject/manage.py migrate %s --settings=%s" % (djangoapp, settings_file))
 
 
 @task
@@ -225,6 +253,15 @@ def deploy(app="fsync", repo="git@github.com:sporty/FruitySync.git"):
 
             # モジュールの更新
             run("pip install -r requirements.txt")
+
+
+@task
+def bootstrap(app="fsync"):
+    """
+    """
+    deploy()
+    migration()
+    setup_all()
 
 
 @task
